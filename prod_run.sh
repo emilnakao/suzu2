e30f89a73fc9a52c5bb7e230dec9cc6a973e4575
Emil Yoshigae Nakao <emilnakao@gmail.com>
2017-07-08 12:08:39 -0300

    Pequenos fixes na configuração do docker

--- prod_run.sh
+++ prod_run.sh
@@ -2,6 +2,4 @@
 
 ./suzu/manage.py syncdb --settings=suzu.settings.docker
 ./suzu/manage.py migrate --all --settings=suzu.settings.docker
-./suzu/manage.py initdb --settings=suzu.settings.docker
 ./suzu/manage.py runserver 0.0.0.0:8000 --settings=suzu.settings.docker
-# uwsgi --ini uwsgi.ini
